02b80b73daba409ddb6191d3a3ec8887d7f20a9e
Vadim Falaleev <fvv14.12.14@gmail.com>
2024-12-10 00:02:27 +0500

    new lasson function

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card, get_date
 
 # импорт функций при помощи абсолютным путём
 
-"""Запрос функций, для маскировки номера карты в числовом значении, маскируется '*'. """
+"""Запрос функций, для обработки информации и счетов."""
 
 # Вывод функции
 if __name__ == "__main__":
 
     # Вызов функции
-    print(get_mask_card_number(7000792289606361))
-    print(get_mask_account(73654108430135874305))
+    print(mask_account_card('Maestro', 1596837868705199))
+    print(mask_account_card('Счет', 64686473678894779589))
+    print(mask_account_card('MasterCard', 7158300734726758))
+    print(mask_account_card('Счет', 35383033474447895560))
+    print(mask_account_card('Visa Classic', 6831982476737658))
+    print(mask_account_card('Visa Platinum', 8990922113665229))
+    print(mask_account_card('Visa Gold', 5999414228426353))
+    print(mask_account_card('Счет', 73654108430135874305))
+
---
+++ src/widget.py
@@ -0,0 +1,35 @@
+from src.masks import get_mask_account, get_mask_card_number # импорт функций
+
+def mask_account_card(name: str, number_card: int) -> str:
+    """Функция обрабатывает информацию о картах и о счетах"""
+
+    if len(str(number_card)) > 16:
+
+        # если количество цифр номера больше 16, это счет
+
+        number_card = get_mask_account(number_card) # шифрует номер счета
+
+    else:
+
+        number_card = get_mask_card_number(number_card) # шифрует номер карты
+
+    str_account_card = str(name) + " " + str(number_card)
+
+    return str_account_card
+
+
+def get_date(date: str) -> str:
+
+    """Функция форматирует строку с датой в установленный образец (ДД.ММ.ГГГГ)"""
+
+    format_date = date.split('T')
+
+    new_format_date = format_date[0].split('-')
+
+    new_date = (new_format_date[2] + '.' + new_format_date[1] + '.' + new_format_date[0])
+
+    return new_date
+
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
