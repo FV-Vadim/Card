0ac2d6835a692d4f2dda757e0fe35c4054fbbff9
Vadim Falaleev <fvv14.12.14@gmail.com>
2024-12-17 22:29:49 +0500

    fixes docstring

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,7 @@
 def filter_by_state(list_of_dict: list = [], state: str = "EXECUTED") -> list:
-    """
-    Функция принимает на вход словарь list_of_dict,
+    """Функция принимает на вход словарь list_of_dict,
     фильтрует словарь банковских операций по параметру state,
-    записывает в словарь filtered_list_of_dict фильтрованный словарь.
-    """
+    записывает в словарь filtered_list_of_dict фильтрованный словарь."""
 
     filtered_list_of_dict = []  # пустой словарь для фильтрованных значений
 
@@ -14,12 +12,10 @@ def filter_by_state(list_of_dict: list = [], state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(list_of_dict: list = [], reverse_date: bool = True) -> list:
-    """
-    Функция sort_by_date принимает на вход словарь list_of_dict,
+    """Функция sort_by_date принимает на вход словарь list_of_dict,
     ключ словаря лямбда функция со значением ключа: data
     направление "по возрастанию" или "по убыванию" зависит от булевого значения
-    по умолчанию значение True
-    """
+    по умолчанию значение True"""
 
     return sorted(
         list_of_dict, key=lambda dictionary: dictionary["date"], reverse=reverse_date
