70e2b851bf71c0c51c456ffa5142e85375100e6d
Vadim Falaleev <fvv14.12.14@gmail.com>
2024-12-17 21:13:47 +0500

    updating annotations

--- src/processing.py
+++ src/processing.py
@@ -2,11 +2,13 @@ from typing import Any, Dict, List
 
 
 def filter_by_state(list_of_dict: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
-    """Функция принимает на вход словарь list_of_dict,
+    """
+    Функция принимает на вход словарь list_of_dict,
     фильтрует словарь банковских операций по параметру state,
-    записывает в словарь filtered_list_of_dict фильтрованный словарь."""
+    записывает в словарь filtered_list_of_dict фильтрованный словарь.
+    """
 
-    if list_of_dict is None: # Если словарь пустой
+    if list_of_dict is None:
         list_of_dict = []
     filtered_list_of_dict = []  # пустой словарь для фильтрованных значений
 
@@ -17,12 +19,14 @@ def filter_by_state(list_of_dict: List[Dict[str, Any]], state: str = "EXECUTED")
 
 
 def sort_by_date(list_of_dict: List[Dict[str, Any]], reverse_date: bool = True) -> List[Dict[str, Any]]:
-    """Функция sort_by_date принимает на вход словарь list_of_dict,
+    """
+    Функция sort_by_date принимает на вход словарь list_of_dict,
     ключ словаря лямбда функция со значением ключа: data
     направление "по возрастанию" или "по убыванию" зависит от булевого значения
-    по умолчанию значение True"""
+    по умолчанию значение True
+    """
 
-    if list_of_dict is None: # Если словарь пустой
+    if list_of_dict is None:
         list_of_dict = []
     return sorted(
         list_of_dict, key=lambda dictionary_key: dictionary_key["date"], reverse=reverse_date
